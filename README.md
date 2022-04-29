a431014645cca1d75cf60536b93412049067ed36
nwnordahl <nicolaiwnordahl@gmail.com>
2022-04-29 10:29:53 +0200

    Fetch profile information and repos information

---
+++ README.md
@@ -0,0 +1,14 @@
+# Portfolio
+
+A collection of the things I have built
+
+# Idéer
+
+- Legge bio som en snakkeboble på profilbilde
+- Kun hente repos som er hostet på GitHub pages (og som ikke er forket) [*]
+- Sortere etter repos som har blitt oppdatert sist
+- Legge til bilde i readme i all prosjekter, og hente bilde ut
+- Hente annen informasjon fra readme
+- Få et tilfeldig prosjekt
+- Legge til gif som viser prosjektet i bruk
+- Fikse titler i prosjektene (mellomrom i stedet for bindestrek, og stor bokstav for hvert ord)
--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const url = "https://api.github.com/users/nwnordahl";
 const profileCard = document.querySelector("#profile-card");
+const container = document.querySelector(".container");
 
-async function getRepos() {
+async function getProfileInformation() {
   const response = await fetch(url);
-  const result = await response.json();
-
-  console.log(result);
+  return await response.json();
 
   profileCard.innerHTML = `
   <h1>${result.name}</h1>
@@ -13,4 +12,40 @@ async function getRepos() {
   <img src="${result.avatar_url}" />`;
 }
 
-getRepos();
+async function getRepos() {
+  const response = await fetch(
+    "https://api.github.com/users/nwnordahl/repos?per_page=100"
+  );
+  return await response.json();
+}
+
+getProfileInformation().then((profileInformation) => {
+  profileCard.innerHTML = `
+  <h1>${profileInformation.name}</h1>
+  <p>${profileInformation.bio}</p>
+  <img src="${profileInformation.avatar_url}" />`;
+});
+
+getRepos().then((repos) => {
+  for (let i = 0; i < repos.length; i++) {
+    const { name, description, homepage, html_url, fork } = repos[i];
+
+    if (homepage && !fork) {
+      console.log(repos[i]);
+      container.innerHTML += `
+    <div id="project-card" class="card">
+      <h1>${name}</h1>
+      <img src="#" />
+      <p>${description}</p>
+      <div class="buttons">
+        <a class="button" href="${homepage}" target="_blank">
+          Se app
+        </a>
+        <a class="button" href="${html_url}" target="_blank">
+          Se kode
+        </a>
+      </div>
+    </div>`;
+    }
+  }
+});
